3bdf4e81c8aef726138da18396d6d3f20eb2e72d
Shamayal Syed <shamayalsyed9@gmail.com>
2023-08-28 02:11:54 -0400

    comments

--- practice.js
+++ practice.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+console.log("Hello, World!"); // This is a single-line comment
+
+// console.log("Hello, World!"); This is also a single-line comment
+
+/*
+This is a multi-line comment 
+
+console.log("Hello, World!"); 
+
+*/
