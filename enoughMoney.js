834a91e02f952b143ab76eac67bfb587bd66974c
Shamayal Syed <shamayalsyed9@gmail.com>
2023-08-30 22:20:57 -0400

    flow chart example if statement and ternary

---
+++ enoughMoney.js
@@ -0,0 +1,9 @@
+let enoughMoney = true;
+
+if (enoughMoney) {
+  console.log('Buy the item!');
+} else {
+  console.log("Don't buy the item.");
+}
+
+console.log(enoughMoney ? 'Buy the item!' : "Don't buy the item.")
