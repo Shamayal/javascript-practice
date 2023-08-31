42082cecfabdfac6ae05717886a61aefbd806c82
Shamayal Syed <shamayalsyed9@gmail.com>
2023-08-30 22:24:47 -0400

    type coercion questions

---
+++ typeCoercion.js
@@ -0,0 +1,24 @@
+console.log('Question 1')
+console.log(20 == '20');
+console.log('Question 2')
+console.log(20 === '20');
+console.log('Question 3')
+console.log('apples' == 'oranges');
+console.log('Question 4')
+console.log('apples' !== 'oranges');
+console.log('Question 5')
+console.log(true == 1);
+console.log('Question 6')
+console.log(true === 1);
+console.log('Question 7')
+console.log(false == 0);
+console.log('Question 8')
+console.log(false === 0);
+console.log('Question 9')
+console.log('hello' == 'Hello');
+console.log('Question 10')
+console.log('Hello' == 'Hello');
+console.log('Question 11')
+console.log(null == undefined);
+console.log('Question 12')
+console.log(null === undefined); // type coercion
