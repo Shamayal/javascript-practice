8aba2f6c9effb0ea98bf4ae031462c55a257901f
Shamayal Syed <shamayalsyed9@gmail.com>
2023-08-30 22:25:00 -0400

    answers to additional practice questions

---
+++ homework.js
@@ -0,0 +1,64 @@
+// Clothing Size
+let shirtWidth = 18;
+let shirtLength = 29;
+let shirtSleeve = 8.47;
+
+if ((shirtWidth >= 18 && shirtWidth < 20) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve >= 8.13 && shirtSleeve <8.38)) {
+  console.log("S");
+} else if ((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve <8.63)) {
+  console.log("M");
+} else if ((shirtWidth === 22) && (shirtLength === 30) && (shirtSleeve === 8.63)) {
+  console.log("L");
+} else {
+  console.log("N/A");
+}
+
+// Checking your Balance
+let balance = 325.3456;
+let checkBalance = false;
+let isActive = true;
+
+if (checkBalance === true) {
+  if (isActive === true && balance > 0) {
+      console.log("Your balance is $" + balance.toFixed(2) + ".")
+  } else if (isActive === true && balance === 0) {
+      console.log("Your account is empty.")
+  } else if (isActive === true && balance < 0) {
+      console.log("Your balance is negative. Please contact bank.")
+  } else console.log("Your account is no longer active.")
+} else console.log("Thank you. Have a nice day!");
+
+// Murder Mystery
+/*
+poison belongs to the ballroom
+book belongs to the library
+pool stick belongs to the billiards room
+knife belongs to the dining room
+
+Ms. Mora was in the ballroom
+Mrs. Ali was in the library
+Mr. Chen was in the billiards room
+Dr. Johnson was in the dining room
+*/
+let weapon = "";
+let solved = false;
+
+let room = "ballroom";
+let suspect = "Ms. Mora";
+
+if (room === "ballroom" && suspect === "Ms. Mora") {
+  weapon = "poison"; solved = true;
+} else if (room === "library" && suspect === "Mrs. Ali") {
+  weapon = "book"; solved = true;
+} else if (room === "billiards room" && suspect === "Mr. Chen") {
+  weapon = "pool stick"; solved = true;
+} else if (room === "dining room" && suspect === "Dr. Johnson") {
+  weapon = "knife"; solved = true;
+} else {
+  solved = false;
+}
+
+// The code below will run only when `solved` is true
+if (solved) {
+console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+}
--- practice.js
+++
@@ -1,4 +0,0 @@
-let radius = 5;
-let area = Math.PI * radius * radius;
-console.log(`The area of a circle with radius ${radius} is approximately ${area.toFixed(2)}.`);
-// output: The area of a circle with radius 5 is approximately 78.54.
