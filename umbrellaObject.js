22cce6b4aabd79f02c0b2abdb573cc16e7184c88
Shamayal Syed <shamayalsyed9@gmail.com>
2023-09-13 12:12:57 -0400

    umbrella example to understand objects

--- umbrellaObject.js
+++ umbrellaObject.js
@@ -1,32 +1,35 @@
-// let umbrella = {
-//   colour: 'orange',
-//   isOpen: false
-// };
-
 let umbrella = {
+  colour: 'orange',
+  isOpen: false
+};
+
+umbrella.colour = 'yellow'
+console.log(umbrella.colour); // returns 'yellow'
+
+let umbrella1 = {
   colour: 'orange', // property
   isOpen: false, // property
   open: function () { // method, this function opens the umbrella if it is closed!
-    if (umbrella.isOpen === true) {
+    if (umbrella1.isOpen === true) {
       return "The umbrella is already open.";
     } else {
-      umbrella.isOpen = true;
+      umbrella1.isOpen = true;
       return "I opened the umbrella.";
     }
   },
   close() { // new way of writing function in an object, this function closes the umbrella if it is open!
-    if (umbrella.isOpen === false) {
+    if (umbrella1.isOpen === false) {
       return "The umbrella is already closed.";
     } else {
-      umbrella.isOpen = false;
+      umbrella1.isOpen = false;
       return "I closed the umbrella.";
     }
   }
 };
 
-console.log(umbrella.open()); // I opened the umbrella, and isOpen is now true
-console.log(umbrella);
+console.log(umbrella1.open()); // I opened the umbrella, and isOpen is now true
+console.log(umbrella1);
 
-for (let key in umbrella) {
-  console.log(`${key}: ${umbrella[key]}`);
+for (let key in umbrella1) {
+  console.log(`${key}: ${umbrella1[key]}`);
 }
